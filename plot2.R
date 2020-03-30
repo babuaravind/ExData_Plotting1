8826164883b00762727de67a3d45368739b3bbb6
babuaravind <itsbabuaravind@gmail.com>
2020-03-30 18:12:42 +0530

    Plotting parameters adjusted to fit the diagram.

--- plot2.R
+++ plot2.R
@@ -6,7 +6,7 @@ data <- read.table('household_power_consumption.txt', header=TRUE, sep=";",col.n
 # Subsetting data from 2007-02-01 to 2007-02-02
 data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
 
-# Plots a histogram
+# Plotting function
 graph <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 plot(graph, data$Global_active_power, xlab="", ylab="Global Active Power (kilowatts)",type="l")
 # Saves a png copy
