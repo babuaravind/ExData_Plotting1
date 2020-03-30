b2d8c128db9b91e388df7e97e6d174ead4aee388
babuaravind <itsbabuaravind@gmail.com>
2020-03-30 18:41:13 +0530

    Changed color of lines.

--- plot3.R
+++ plot3.R
@@ -9,7 +9,7 @@ data <- data[data$Date %in% c("1/2/2007", "2/2/2007"),]
 # Plotting function
 graph <- strptime(paste(data$Date, data$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 plot(graph, data$Sub_metering_1, type="l", xlab="", ylab="Energy Submetering")
-legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col= c('black', 'red', 'blue'))
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col= c('black', 'red', 'blue'), lty=, lwd='2.2', bty='o')
 lines(graph, data$Sub_metering_2, type='l', col='red')
 lines(graph, data$Sub_metering_3, type='l', col='blue')
 
